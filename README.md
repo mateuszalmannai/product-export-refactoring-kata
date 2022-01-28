75c4e3df8e37f94c5bff1e8250e1c599665dd9f5
mateusz <mateuszalmannai@googlemail.com>
2022-01-28 22:19:21 +0100

    100% coverage

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Product-Export-Refactoring-Kata 
 - by Emily Bache
 - https://github.com/emilybache/Product-Export-Refactoring-Kata
+
+# Setup
+- Edit Run Configuration > Use Tracing
+- Preferences > Coverage
+  - Replace Active Suites with new one 
+  - Uncheck activate coverage view
--- src/test/java/codingdojo/XMLExporterTest.java
+++ src/test/java/codingdojo/XMLExporterTest.java
@@ -1,15 +1,51 @@
 package codingdojo;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
+import org.approvaltests.scrubbers.RegExScrubber;
 import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+
+import static codingdojo.SampleModelObjects.*;
 
 public class XMLExporterTest {
 
   @Test
-  public void testSomething() {
+  public void testExportFullTest() {
+    Collection<Order> orders = List.of(RecentOrder, OldOrder);
+    String xml = XMLExporter.exportFull(orders);
 
-    // TODO: implement this test
-
+    Approvals.verifyXml(xml);
   }
 
+  @Test
+  public void testExportTaxDetails() {
+    List<Order> orders = List.of(RecentOrder, OldOrder);
+    String xml = XMLExporter.exportTaxDetails(orders);
+
+    Approvals.verifyXml(xml);
+  }
+
+  @Test
+  public void testExportStore() {
+    Store store = FlagshipStore;
+    String xml = XMLExporter.exportStore(store);
+
+    Approvals.verifyXml(xml);
+  }
+
+  @Test
+  public void testExportHistory() {
+    List<Order> orders = List.of(RecentOrder, OldOrder);
+    String xml = XMLExporter.exportHistory(orders);
+    String regex = "createdAt='[^']+'";
+    Approvals.verifyXml(xml, new Options(new RegExScrubber(
+        "createdAt=\"[^\"]+\"",
+        "createdAt=\"2018-09-20T00:00Z\""
+    )));
+
+  }
 }
 
